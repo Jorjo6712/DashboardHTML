add062917d02a761d2de8c363b16afb62b47ee00
MagnusHLund <magnusherringelund@gmail.dk>
2023-09-07 12:40:58 +0200

    javascript updates

--- games.js
+++ games.js
@@ -1,16 +1,64 @@
+const loadGames = document.getElementById("loadGame");
+const content = document.getElementById("gameContent");
 const url = 'https://epic-free-games.p.rapidapi.com/epic-free-games';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4099ab78f2msh0eb22cfce6cef26p1cbbd7jsn21da45057ae1',
-		'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
-	}
+const ApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '4099ab78f2msh0eb22cfce6cef26p1cbbd7jsn21da45057ae1',
+        'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
+    }
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function GetData() {
+    remove();
+    try {
+        const response = await fetch(url, ApiOptions);
+        const result = await response.json(); // Use response.json() to parse JSON directly
+        console.log(result);
+
+        // Loop through the games and call UpdateHTML() for each game
+        result.forEach(item => {
+            UpdateHTML(item);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+function UpdateHTML(item) {
+    const div = document.createElement("div");
+    div.className = "game";
+    content.appendChild(div);
+
+    const name = document.createElement("h2");
+    name.className = "gameName";
+    name.textContent = item.name;
+    div.appendChild(name);
+
+    const price = document.createElement("h2");
+    price.className = "gamePrice";
+    const decimalPrice = item.originalPrice / 100;
+    price.textContent = `Original price: ${decimalPrice} ${item.currencyCode}`;
+    div.appendChild(price);
+
+    const image = document.createElement("img");
+    image.src = item.offerImageTall;
+    image.width = 400;
+    image.height = 500;
+    image.className = "gameImage";
+    div.appendChild(image);
+}
+
+function remove() {
+    // Selects all elements with the game class, as an array
+    let get = document.querySelectorAll('.game');
+
+    // Goes through each element and removes them
+    get.forEach(element => {
+        element.remove();
+    });
+}
+
+// Calls function once user presses the load button
+loadGames.addEventListener("click", GetData);
--- weatherapi.js
+++ weatherapi.js
@@ -1,57 +1,84 @@
+const weatherContent = document.getElementById("weatherContent");
+
 function apiFetchData() {
     const request = new XMLHttpRequest();
-    var weatherDOM = document.getElementsByClassName("weatherContent");
-  
+
     request.open("GET", "http://api.weatherstack.com/current?access_key=1959fdf3f11c80e464902d618594bedc&query=Copenhagen");
-    
+
     request.send();
-    
+
     request.onload = () => {
-      if (request.status === 200) {
-        var apiResponse = JSON.parse(request.responseText);
-        var city = apiResponse.location.name;
-        var country = apiResponse.location.country;
-        var region = apiResponse.location.region;
-        var localtime = apiResponse.location.localtime;
-        var temperature = apiResponse.current.temperature;
-        var weather_icons = apiResponse.current.weather_icons;
-        var weather_descriptions = apiResponse.current.weather_descriptions;
-        var wind_speed = apiResponse.current.wind_speed;
-        var wind_degree = apiResponse.current.wind_degree;
-        var humidity = apiResponse.current.humidity;
-        var wind_direction = apiResponse.current.wind_dir;
-        var pressure = apiResponse.current.pressure;
-        var feelslike = apiResponse.current.feelslike;
-  
-        var weatherIconUrl = apiResponse.current.weather_icons[0];
-  
-        var weatherIconImg = new Image();
-  
-        weatherIconImg.src = weatherIconUrl;
-  
-        weatherDOM[0].appendChild(weatherIconImg);
-  
-        var textToShow = 
-        `City: ${city}, 
-        Country: ${country}, 
-        Region: ${region}, 
-        Local Time: ${localtime},
-        Temperature: ${temperature}°, 
-        ${weather_descriptions}, 
-        Wind Speed: ${wind_speed}m/s,
-        Wind Degree: ${wind_degree},
-        Humidity: ${humidity}%,
-        Wind Direction: ${wind_direction},
-        Pressure: ${pressure},
-        Feels like: ${feelslike}°`;
-        
-        weatherDOM[0].textContent = textToShow;
-  
-        console.log(weatherIconImg);
-  
-  
-      } else {
-        console.error('Failed to fetch weather data');
-      }
+        if (request.status === 200) {
+            // Parse text response to json and output to console
+            var apiResponse = JSON.parse(request.responseText);
+            console.log(apiResponse);
+
+            // Output variables to HTML, by creating elements
+            const icon = document.createElement("img");
+            const time = document.createElement("h2");
+            const location = document.createElement("h2");
+            const description = document.createElement("h2");
+            const temperature = document.createElement("h2");
+            const feelslike = document.createElement("h2");
+            const wind = document.createElement("h2");
+            const uv = document.createElement("h2");
+            const humidity = document.createElement("h2");
+            const visibility = document.createElement("h2");
+
+            // Gives values to variables, from the API
+            location.textContent = `Location: ${apiResponse.location.country}, ${apiResponse.location.name}`;
+            time.textContent = `time measured: ${apiResponse.location.localtime}`;
+            temperature.textContent = `Temperature: ${apiResponse.current.temperature}°`;
+            icon.src = apiResponse.current.weather_icons;
+            wind.textContent = `Wind: ${apiResponse.current.wind_speed}m/s`;
+            visibility.textContent = `Visibility ${apiResponse.current.visibility}`;
+            feelslike.textContent = `Feels like: ${apiResponse.current.feelslike}°`;
+            humidity.textContent = `Humidity: ${apiResponse.current.humidity}%`;
+            description.textContent = apiResponse.current.weather_descriptions;
+            uv.textContent = `UV index: ${apiResponse.current.uv_index}`;
+            console.log(visibility);
+
+            // Make each element a child of the div called weatherContent
+            weatherContent.appendChild(icon);
+            weatherContent.appendChild(time);
+            weatherContent.appendChild(location);
+            weatherContent.appendChild(description);
+            weatherContent.appendChild(temperature);
+            weatherContent.appendChild(feelslike);
+            weatherContent.appendChild(wind);
+            weatherContent.appendChild(uv);
+            weatherContent.appendChild(humidity);
+            weatherContent.appendChild(visibility);
+
+
+            /*
+                  var weatherIconImg = new Image();
+            
+                  weatherIconImg.src = weatherIconUrl;
+          
+                  weatherDOM[0].appendChild(weatherIconImg);
+            
+                  var textToShow = 
+                  `City: ${city}, 
+                  Country: ${country}, 
+                  Region: ${region}, 
+                  Local Time: ${localtime},
+                  Temperature: ${temperature}°, 
+                  ${weather_descriptions}, 
+                  Wind Speed: ${wind_speed}m/s,
+                  Wind Degree: ${wind_degree},
+                  Humidity: ${humidity}%,
+                  Wind Direction: ${wind_direction},
+                  Pressure: ${pressure},
+                  Feels like: ${feelslike}°`;
+                  
+                  weatherDOM[0].textContent = textToShow; */
+
+
+        } else {
+            console.error('Failed to fetch weather data');
+        }
     }
-  }
+}
+
+apiFetchData();
